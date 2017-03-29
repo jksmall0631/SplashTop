f7bc29db92af3956a18725b7a9dff84009d3545d
Noah Peden <noahpeden@gmail.com>
2017-03-29 08:39:33 -0600

    added first tile to home js

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import styles from './Home.css';
+import natGeo from '../assets/nat-geo.jpg'
 
 
 export default class Home extends Component {
@@ -9,7 +10,15 @@ export default class Home extends Component {
     return (
       <div>
         <div className={styles.container} data-tid="container">
-          <h1>SplashTop</h1>
+          <div className={styles.natgeo}>
+              <div className={styles.hovereffect}>
+                  <img className={styles.imgresponsive} src={natGeo} alt=""/>
+                  <div className={styles.overlay}>
+                     <h2>National Geographic</h2>
+                     <a className={styles.info} href="#">Click for photos</a>
+                  </div>
+              </div>
+          </div>
         </div>
       </div>
     );
