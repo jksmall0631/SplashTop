fe97ecc7ccecb2ffbcd4e201174369b81631f502
Joshua Small <jksmall0631@gmail.com>
2017-03-29 16:51:25 -0600

    create download photos to fs functionality

--- app/components/Thumbnail.js
+++ app/components/Thumbnail.js
@@ -1,9 +1,29 @@
 import React, {Component} from 'react'
+import electron from 'electron'
+const remote = require('electron').remote
+// const { download } = remote.require('app')
+const ipcRenderer = require('electron').ipcRenderer
 
 export default class Thumbnail extends Component {
+  savePic() {
+    const url = this.props.urls.full
+    const fileName = this.props.id + '.png'
+
+    ipcRenderer.send('save-photo', { fileName, url })
+
+    ipcRenderer.once('save-photo-reply', (event, arg) => {
+      console.log(arg)
+    })
+
+    // console.log('savePic()')
+    // // download( this.props.urls.full, this.props.urls.id + '.png', () => {
+    //   console.log('boom')
+    // })
+  }
+
   render() {
     return (
-      <img src={this.props.urls.thumb} />
+      <img src={this.props.urls.thumb} onClick={() => this.savePic()} />
     )
   }
 }
