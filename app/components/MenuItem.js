2b875f250b0440c0000e87209380b33e54fc2673
Noah Peden <noahpeden@gmail.com>
2017-04-02 17:09:33 -0600

    hoverable tile working for unsplash

--- app/components/MenuItem.js
+++ app/components/MenuItem.js
@@ -5,18 +5,20 @@ import styles from './MenuItem.css'
 export default class MenuItem extends Component {
   render() {
     return (
-      <figure className={styles.figure}>
-        <Link
-          to={this.props.route}
-          className={styles.title}
-          onClick={() => this.props.handleClick()}
-          >
-          <img className={styles.img} src={this.props.src} alt='img' />
-        </Link>
-            <figcaption className={styles.figcaption}>
-            <h2 className={styles.linkText}>{this.props.name}</h2>
-        </figcaption>
-      </figure>
+      <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+    <div className={styles.hovereffect}>
+        <img className={styles.imgresponsive} src='https://source.unsplash.com/random/450x350' />
+        <div className={styles.overlay}>
+           <h2>UnSplash</h2>
+           <Link
+             className={styles.info}
+             to={this.props.route}
+             onClick={() => this.props.handleClick()}
+             >Click for backgrounds
+             </Link>
+        </div>
+    </div>
+</div>
     )
   }
 }
